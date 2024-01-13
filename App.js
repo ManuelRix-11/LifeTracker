17cf58fc3566af026b181e21f5b1d227fc8f93e3
ManuelRix <manuelrixworkmail@gmail.com>
2024-01-13 23:37:44 +0100

    Creazione progetto

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import PagerView from 'react-native-pager-view'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <PagerView initialPage={0}>
+        <View key={1}>
+
+        </View>
+        <View key={2}>
+
+        </View>
+      </PagerView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
---
+++ screens/ToDoScreen.js
@@ -0,0 +1,9 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const ToDoScreen = () =>{
+    return(
+        <View>
+            
+        </View>
+    )
+}
